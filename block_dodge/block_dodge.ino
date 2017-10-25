15cd9056f02f2974c952f0bebe71d0a912ee861e
robert_kuramshin <robert.kuramshin98@gmail.com>
2017-10-25 01:49:54 -0400

    starting game logic

--- block_dodge/block_dodge.ino
+++ block_dodge/block_dodge.ino
@@ -21,27 +21,34 @@ bool frame_buffer[HEIGHT][WIDTH];
 
 bool buffer_ready = false;
 
-void setup() {
+void setup()
+{
   tlc5940.init();
   pinMode(PB3, INPUT);
 
   last_tick = millis();
 }
 
-
-void loop () {
-  if (millis() - last_tick < TICK) {
-    if(!buffer_ready){
-        update();
+void loop()
+{
+  if (millis() - last_tick < TICK)
+  {
+    if (!buffer_ready)
+    {
+      update();
     }
-    if(keyPressed()) {
+    if (keyPressed())
+    {
       //move
       last_tick = millis();
       refreshBuffer();
     }
     paint();
-  } else {
-    if(buffer_ready){
+  }
+  else
+  {
+    if (buffer_ready)
+    {
       refreshBuffer();
       buffer_ready = false;
     }
@@ -49,87 +56,133 @@ void loop () {
     paint();
   }
 }
-void update() {
-  //move everything down
+void update()
+{
+  long rand = random(0, 12);
+  bool[] row = {false, false, false};
+  switch (rand)
+  {
+  case 0:
+    row = {true, false, false};
+    break;
+  case 1:
+    row = {true, true, false};
+    break;
+  case 2:
+    row = {false, true, false};
+    break;
+  case 3:
+    row = {false, true, true};
+    break;
+  case 4:
+    row = {false, false, true};
+    break;
+  }
+
+  for (int y = HEIGHT - 2; y >= 0; i--)
+  {
+    for (int x = 0; x < 3; x++)
+    {
+      if (valid(x, y))
+      {
+        frame_buffer[x][y + 1] = frame_buffer[x][y];
+      }
+    }
+  }
   //update top row
-  buufer_ready = true;
+  buffer_ready = true;
 }
-void paint() {
-  for (int i = 0 ; i < HEIGHT; i++) {
-    for (int j = 0 ; j < WIDTH; j++) {
-      if (frame[i][j]) {
+void paint()
+{
+  for (int i = 0; i < HEIGHT; i++)
+  {
+    for (int j = 0; j < WIDTH; j++)
+    {
+      if (frame[i][j])
+      {
         setLED(i, j, true);
-      } else {
+      }
+      else
+      {
         setLED(i, j, false);
       }
     }
   }
   tlc5940.update();
 }
-void setFrameBuffer(int x, int y, bool state) {
-  if(valid(x,y)){
-    frame_buffer[x][y] = state;
-  }
-}
-
-void clearFrameBuffer() {
-    for (int i = 0 ; i < HEIGHT; i++) {
-    for (int j = 0 ; j < WIDTH; j++) {
+void clearFrameBuffer()
+{
+  for (int i = 0; i < HEIGHT; i++)
+  {
+    for (int j = 0; j < WIDTH; j++)
+    {
       frame_buffer[i][j] = false;
     }
   }
 }
-void refreshBuffer() {
-  for (int i = 0 ; i < HEIGHT; i++) {
-    for (int j = 0 ; j < WIDTH; j++) {
+void refreshBuffer()
+{
+  for (int i = 0; i < HEIGHT; i++)
+  {
+    for (int j = 0; j < WIDTH; j++)
+    {
       frame[i][j] = frame_buffer[i][j];
     }
   }
 }
-void setLED(int x, int y, bool value) {
-  int index = x * WIDTH + y;
+void setLED(int x, int y, bool value)
+{
+  int index = y * WIDTH + x;
   index = mapping[index];
-  if (value) {
+  if (value)
+  {
     tlc5940.set(index, 2000);
-  } else {
+  }
+  else
+  {
     tlc5940.set(index, 0);
   }
 }
-bool valid(int x, int y) {
-  if (x < 0 || x >= HEIGHT || y < 0 || y >= WIDTH) {
+bool valid(int x, int y)
+{
+  if (y < 0 || y >= HEIGHT || x < 0 || x >= WIDTH)
+  {
     return false;
   }
   return true;
 }
-
-int buttonStatus() {
+int buttonStatus()
+{
   int value = analogRead(PB3);
   value = value / (1024 / 15);
   int key = 0;
-  switch (value) {
-    case 11:
-      key = 2;
-      break;
-    case 13:
-      key = 1;
-      break;
-    case 12:
-      key = 3;
+  switch (value)
+  {
+  case 11:
+    key = 2;
+    break;
+  case 13:
+    key = 1;
+    break;
+  case 12:
+    key = 3;
   }
   return key;
 }
-
-bool keyPressed() {
+bool keyPressed()
+{
   int reading = buttonStatus();
-  if (reading != last_stroke) {
+  if (reading != last_stroke)
+  {
     last_press = millis();
   }
-  if ((millis() - last_press) > debounce_delay) {
-    if (reading != key_stroke) {
+  if ((millis() - last_press) > debounce_delay)
+  {
+    if (reading != key_stroke)
+    {
       key_stroke = reading;
       return true;
     }
   }
   return false;
 }
-
